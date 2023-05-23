d255f57ee630bc4f88758c45dc7e483db7dad782
Jacob Hein <jacobhein1892@gmail.com>
2023-05-22 18:57:10 -0700

    added main file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// A Medical Information System is designed to store and organize patient medical information, such as demographic and clinical data, lab results, and other relevant information using a C++ programming language. The system is designed to allow easy expansion, and efficient and secure payments using object-oriented programming techniques to ensure the data is stored, retrieved, and managed efficiently. The system can use encryption and authentication techniques to ensure the data is kept secure and private for a particular patient. 
+
+// Technologies Required: C++ programming language, Object Oriented Programming (OOP), Encryption & Authentication and Database Management System (DBMS).
+
+
+
+int main(){
+    return 0;
+}
